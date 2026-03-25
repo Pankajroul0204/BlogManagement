8df8ea7889addfaaf229cb63073e07251d052fb2
Dev@Cyber3 <cymationdeveloper@gmail.com>
2026-03-25 19:29:04 +0530

    CSRF_TRUSTED_ORIGINS

--- BlogManagement/BlogManagement/settings.py
+++ BlogManagement/BlogManagement/settings.py
@@ -112,6 +112,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://blogmanagement-h7wx.onrender.com"
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
